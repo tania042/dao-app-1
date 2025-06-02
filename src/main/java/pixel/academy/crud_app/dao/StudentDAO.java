dfb918cd592d99efcb8f4567480d5a49311be045
Tatiana Ceban <tatiana.ceban.19@bk.ru>
2025-06-02 17:49:24 +0300

    Add changes

---
+++ src/main/java/pixel/academy/crud_app/dao/StudentDAO.java
@@ -0,0 +1,11 @@
+package pixel.academy.crud_app.dao;
+
+import pixel.academy.crud_app.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student theStudent);
+
+
+
+}
---
+++ src/main/java/pixel/academy/crud_app/dao/StudentDAOImplementation.java
@@ -0,0 +1,30 @@
+package pixel.academy.crud_app.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import pixel.academy.crud_app.entity.Student;
+
+@Repository
+public class StudentDAOImplementation implements StudentDAO {
+
+    // Cimp pentru EntityManager (va fi utilizat pentru interactiunea cu baza de date)
+    private EntityManager entityManager;
+
+    // Injectarea EntityManager prin constructor (practica recomandata pentru testabilitatea si modularitate)
+    @Autowired
+    public StudentDAOImplementation(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+
+    }
+
+
+
+}
