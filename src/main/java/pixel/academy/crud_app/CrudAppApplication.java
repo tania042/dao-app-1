9483cd0e80b1553adaf8abf59a01ffbe33b76d0a
Tatiana Ceban <tatiana.ceban.19@bk.ru>
2025-06-02 17:22:13 +0300

    Initial commit

---
+++ src/main/java/pixel/academy/crud_app/CrudAppApplication.java
@@ -0,0 +1,25 @@
+package pixel.academy.crud_app;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.beans.BeanProperty;
+
+@SpringBootApplication
+public class CrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAppApplication.class, args);
+	}
+
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+
+		return runner -> {
+			System.out.println("Hello, world!");
+		};
+	}
+}
